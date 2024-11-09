48e902ceeaf09c7f68c71e3c7c95a94f61d0109c
ever <eber.penaloza10@gmail.com>
2024-11-08 19:36:20 -0500

    se edito algunas mejorar para el código, respecto a usabilidad

--- vehiculo.cpp
+++ vehiculo.cpp
@@ -11,7 +11,7 @@ class Vehiculo
 	public:
 		Vehiculo(string, string, string, int);
 		void mostrarDato();
-		void cambiarVelocidad(float);
+		void cambiarVelocidad(int);
 };
 Vehiculo::Vehiculo(string _numeroPlaca, string _color, string _marca, int _velocimetro)
 {
@@ -22,34 +22,37 @@ Vehiculo::Vehiculo(string _numeroPlaca, string _color, string _marca, int _veloc
 }
 void Vehiculo::mostrarDato()
 {
-	cout<<"numero de placa:"<<numeroPlaca<<endl;
-	cout<<"color "<<color<<endl;
-	cout<<"marca"<<marca<<endl;
-	cout<<"velocimetro"<<velocimetro<<endl;
+	cout<<"              MOSTRAR RESULTADOS             "<<endl;
+    cout<<"---------------------------------------------"<<endl;
+	cout<<"NUMERO DE PLACA: "<<numeroPlaca<<endl;
+	cout<<"COLOR: "<<color<<endl;
+	cout<<"MARCA: "<<marca<<endl;
+	cout<<"VELOCIMETRO: "<<velocimetro<<endl;
 }
-void Vehiculo::cambiarVelocidad(float nuevaVelocidad)
+void Vehiculo::cambiarVelocidad(int nuevaVelocidad)
 {
 	velocimetro=nuevaVelocidad;
 }
 class Automovil:public Vehiculo{
-	private:
+	protected:
 		int cantidadPuertas;
 		int cantidadPasajeros;
 	public:
-		Automovil(int, int);
+		Automovil(string, string, string, int, int, int);
 		void mostrarAutomovil();
 		void subirPasajero(int);
 		void bajarPasajero(int);
 };
-Automovil::Automovil(int _cantidadPuertas, int _cantidadPasajeros):Vehiculo(numeroPlaca, color, marca, velocimetro){
+Automovil::Automovil(string _numeroPlaca, string _color, string _marca, int _velocimetro, int _cantidadPuertas, int _cantidadPasajeros)
+    : Vehiculo(_numeroPlaca, _color, _marca, _velocimetro){
 	cantidadPuertas=_cantidadPuertas;
 	cantidadPasajeros=_cantidadPasajeros;
 }
 void Automovil::mostrarAutomovil()
 {
 	mostrarDato();
-	cout<<"puertas"<<cantidadPuertas<<endl;
-	cout<<"pasajeros"<<cantidadPasajeros<<endl;
+	cout<<"PUERTAS: "<<cantidadPuertas<<endl;
+	cout<<"PASAJEROS: "<<cantidadPasajeros<<endl;
 }
 void Automovil::subirPasajero(int cantidad)
 {
@@ -67,31 +70,61 @@ void Automovil::bajarPasajero(int cantidad)
 	}
 }
 class Camion:public Vehiculo{
-	private:
-		int carga;
+	protected:
+		float carga;
 	public:
-		Camion(int);
+		Camion(string, string, string, int, float);
 		void mostrarCamion();
-		void cambiarCarga(int);
+		void cambiarCarga(float);
 };
-Camion::Camion(int _carga)
-{
+Camion::Camion(string _numeroPlaca, string _color, string _marca, int _velocimetro, float _carga)
+    : Vehiculo(_numeroPlaca, _color, _marca, _velocimetro){
 	carga=_carga;
 }
 void Camion::mostrarCamion()
 {
 	mostrarDato();
-	cout<<"carga:"<<carga<<" toneladas"<<endl;
+	cout<<"CARGA:"<<carga<<" toneladas"<<endl;
 }
-void Camion::cambiarCarga(int nuevaCarga)
+void Camion::cambiarCarga(float nuevaCarga)
 {
 	carga=nuevaCarga;
 }
 int main()
 {
-	Automovil au1("234d", "rojo", "toyota", 160);
-	au1.mostrarAutomovil();
-	au1.subirPasajero(2);
-	au1.mostrarAutomovil();
-	return 0;
+	string numeroPlaca, color, marca;
+	int velocimetro;
+	int cantidadPuertas, cantidadPasajeros;
+	cout<<"--------------AUTOMOVIL--------------"<<endl;
+	cout<<"INGRESAR PLACA:"; cin>>numeroPlaca;
+	cout<<"INGRESAR COLOR:"; cin>>color;
+	cout<<"INGRESAR MARCA:"; cin>>marca;
+	cout<<"INGRESAR VELOCIMETRO:"; cin>>velocimetro;
+	cout<<"INGRESAR CANTIDAD DE PUERTAS:"; cin>>cantidadPuertas;
+	cout<<"INGRESAR CANTIDAD DE PASAJEROS:"; cin>>cantidadPasajeros; 
+    Automovil au1(numeroPlaca, color, marca, velocimetro, cantidadPuertas, cantidadPasajeros);
+    au1.mostrarAutomovil();
+    int subirPasajeros;
+    int bajarPasajeros;
+    cout<<"INGRESE LA CANTIDAD DE PASAJEROS A SUBIR:"; cin>>subirPasajeros;
+    au1.subirPasajero(subirPasajeros);
+    au1.mostrarAutomovil();
+    cout<<"INGRESE LA CANTIDAD DE PASAJEROS A BAJAR:"; cin>>bajarPasajeros;
+    au1.bajarPasajero(bajarPasajeros);
+    au1.mostrarAutomovil();
+    float carga;
+    cout<<"---------------CAMION---------------"<<endl;
+    cout<<"INGRESAR PLACA:"; cin>>numeroPlaca;
+	cout<<"INGRESAR COLOR:"; cin>>color;
+	cout<<"INGRESAR MARCA:"; cin>>marca;
+	cout<<"INGRESAR VELOCIMETRO:"; cin>>velocimetro;
+	cout<<"INGRESE LA CARGA EN TONELADAS:"; cin>>carga;
+	Camion camion1(numeroPlaca, color, marca, velocimetro, carga);
+	camion1.mostrarCamion();
+	float nuevacarga;
+	cout<<"INGRESE LA NUEVA CARGA:"; cin>>nuevacarga;
+	camion1.cambiarCarga(nuevacarga);
+	camion1.mostrarCamion();
+    
+    return 0;
 }
